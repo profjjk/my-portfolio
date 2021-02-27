5f92e55aae5d8a78c0b9625c33675fcf0d775891
Jordan Kelly <profjjk@gmail.com>
2021-02-27 12:18:54 -0800

    create & style home page header

---
+++ my-react-site/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="row">
+      <div className="col-12 p-5 d-flex align-items-end">
+        <div>
+          <h4 className="fs-2">Web Developer</h4>
+          <h1 className="display-1">JORDAN KELLY</h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
--- my-react-site/src/pages/Home.js
+++ my-react-site/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    
+    <h1>Hello World!</h1>
   )
 }
 
